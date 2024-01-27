a52fe4b8600a056ae885b17742327ec472484d5d
Alisa <aesonse@yandex.ru>
2024-01-28 01:15:52 +0300

    HW2

---
+++ HW2.py
@@ -0,0 +1,7 @@
+n = int(input())
+for i in range(1, n + 1):
+    print("\n")
+    for j in range(1, n + 1):
+        print(i, "*", j, "=", i * j)
+
+#Выбранная парадигма: структурная, императивная, 
